0691bc4e53192f1d134b250e622f204eb8f745fb
kuhachi <n-hieu@plott.co.jp>
2024-02-05 23:30:21 +0700

    28/01/2024: Done

--- 91-meals-app/screens/MealsOverviewScreen.js
+++ 91-meals-app/screens/MealsOverviewScreen.js
@@ -1,14 +1,30 @@
-import { View, Text, StyleSheet } from 'react-native';
-import { MEALS } from '../data/dummy-data'
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { MEALS } from '../data/dummy-data';
+import MealItem from '../components/MeaItem'
 
 function MealsOverviewScreen ({route}) {
    const cateId = route.params?.categoryId || "";
+
+   const displayedMeals = MEALS.filter((mealItem) => {
+       return mealItem.categoryIds.indexOf(catId) >= 0;
+   });
+
+   console.log({displayedMeals});
+
+   function  renderMealItem(itemData) {
+       return <MealItem title={itemData.item.title}/>
+    }
 
     return (
         <View style={styles.container}>
             <Text>
                 Meals Overview Screen - {cateId}
             </Text>
+            <FlatList
+                data={displayedMeals}
+                keyExtractor={(item) => item.id}
+                renderItem={renderMealItem}
+            />
         </View>
     );
 }

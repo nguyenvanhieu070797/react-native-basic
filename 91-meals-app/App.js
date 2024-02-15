da52e6169b77bca0875c75cea2c59ab55dc7591e
kuhachi <n-hieu@plott.co.jp>
2024-02-15 21:31:57 +0700

    test commit

--- 91-meals-app/App.js
+++ 91-meals-app/App.js
@@ -2,12 +2,42 @@ import { StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
+import FavoritesScreen from "./screens/FavoritesScreen";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+    return <Drawer.Navigator
+        screenOptions={{
+            headerStyle: {
+                backgroundColor: '#351401',
+            },
+            headerTintColor: 'white',
+            sceneContainerStyle: {
+                backgroundColor: '#3f2f25',
+            }
+        }}
+    >
+        <Drawer.Screen
+            name="Categories"
+            component={CategoriesScreen}
+            options={{
+                headerBackTitle: "All Categories",
+                title: 'All Categories',
+            }}
+        />
+        <Drawer.Screen
+            name="Favorites"
+            component={FavoritesScreen}
+        />
+    </Drawer.Navigator>
+}
 
 export default function App() {
   return (
@@ -26,14 +56,24 @@ export default function App() {
                       }
                   }}
               >
+                  {/*<Stack.Screen*/}
+                  {/*    name="Categories"*/}
+                  {/*    component={CategoriesScreen}*/}
+                  {/*    options={{*/}
+                  {/*        headerBackTitle: "Back",*/}
+                  {/*        title: 'All Categories',*/}
+                  {/*    }}*/}
+                  {/*/>*/}
+
                   <Stack.Screen
-                      name="Categories"
-                      component={CategoriesScreen}
+                      name="Drawer"
+                      component={DrawerNavigator}
                       options={{
-                          headerBackTitle: "Back",
                           title: 'All Categories',
+                          headerShown: false,
                       }}
                   />
+
                   <Stack.Screen
                       name="MealsOverview"
                       component={MealsOverviewScreen}
--- README.md
+++ README.md
@@ -26,9 +26,7 @@
      - expo install react-native-screens react-native-safe-area-context
      - expo install @react-navigation/native-stack
      - expo install @react-navigation/drawer
-     - expo install @react-navigation/bottom-tabs
      - expo install react-native-gesture-handler react-native-reanimated
+     - expo install @react-navigation/bottom-tabs
      - expo install @expo/vector-icons
-
-
 

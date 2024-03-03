1ba281bf7c036af002107c34a858097bfb8658cc
kuhachi <n-hieu@plott.co.jp>
2024-03-03 22:25:36 +0700

    15/02/2024: test coder

--- 91-meals-app/App.js
+++ 91-meals-app/App.js
@@ -3,12 +3,14 @@ import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { createDrawerNavigator } from '@react-navigation/drawer'
+import {Ionicons} from "@expo/vector-icons";
 
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
 import FavoritesScreen from "./screens/FavoritesScreen";
-import {Ionicons} from "@expo/vector-icons";
+
+import FavoritesContext from './store/context/favorites-context'
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -56,41 +58,43 @@ export default function App() {
   return (
       <>
           <StatusBar style="light"/>
-          <NavigationContainer>
-              <Stack.Navigator
-                  initialRouteName="MealsCategories"
-                  screenOptions={{
-                      headerStyle: {
-                          backgroundColor: '#351401',
-                      },
-                      headerTintColor: "white",
-                      contentStyle: {
-                          backgroundColor: '#3f2f25',
-                      },
+          <FavoritesContext>
+              <NavigationContainer>
+                  <Stack.Navigator
+                      initialRouteName="MealsCategories"
+                      screenOptions={{
+                          headerStyle: {
+                              backgroundColor: '#351401',
+                          },
+                          headerTintColor: "white",
+                          contentStyle: {
+                              backgroundColor: '#3f2f25',
+                          },
 
-                  }}
-              >
-
-                  <Stack.Screen
-                      name="Drawer"
-                      component={DrawerNavigator}
-                      options={{
-                          title: 'All Categories',
-                          headerShown: false,
                       }}
-                  />
+                  >
 
-                  <Stack.Screen
-                      name="MealsOverview"
-                      component={MealsOverviewScreen}
-                  />
+                      <Stack.Screen
+                          name="Drawer"
+                          component={DrawerNavigator}
+                          options={{
+                              title: 'All Categories',
+                              headerShown: false,
+                          }}
+                      />
 
-                  <Stack.Screen
-                      name="MealDetail"
-                      component={MealDetailScreen}
-                  />
-              </Stack.Navigator>
-          </NavigationContainer>
+                      <Stack.Screen
+                          name="MealsOverview"
+                          component={MealsOverviewScreen}
+                      />
+
+                      <Stack.Screen
+                          name="MealDetail"
+                          component={MealDetailScreen}
+                      />
+                  </Stack.Navigator>
+              </NavigationContainer>
+          </FavoritesContext>
       </>
   );
 }
--- 91-meals-app/screens/MealDetailScreen.js
+++ 91-meals-app/screens/MealDetailScreen.js
@@ -1,5 +1,5 @@
 import {View, Text, Image, StyleSheet, ScrollView} from 'react-native'
-import {useLayoutEffect} from "react";
+import {useLayoutEffect, useContext} from "react";
 
 import {MEALS} from '../data/dummy-data'
 import MealDetails from "../components/MealDetails"
@@ -7,16 +7,26 @@ import Subtitle from "../components/MealDetail/Subtitle"
 import List from "../components/MealDetail/List"
 import IconButton from "../components/IconButton";
 
+import {FavoritesContext} from '../store/context/favorites-context'
+
 function MealDetailScreen ({route, navigation}) {
+    const favoriteMealsCtx = useContext(FavoritesContext);
+
     const mealId = route.params.mealId;
 
     const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
-    console.log({selectedMeal});
+    const mealIsFavorite = favoriteMealsCtx.ids.includes(mealId);
 
-    function headerButtonPressHandler() {
-        console.log("pressed!!");
+    function changeFavoriteStatusHandle() {
+        if (mealIsFavorite) {
+            favoriteMealsCtx.removeFavorite(mealId);
+        } else {
+            favoriteMealsCtx.addFavorite(mealId);
+        }
     }
+
+    console.log({ids: favoriteMealsCtx.ids, mealId, mealIsFavorite});
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -24,13 +34,13 @@ function MealDetailScreen ({route, navigation}) {
             headerBackTitle: "Back",
             headerRight: () => {
                 return <IconButton
-                    icon={'star'}
+                    icon={mealIsFavorite ? 'star' : 'star-outline'}
                     color={'white'}
-                    onPress={headerButtonPressHandler}
+                    onPress={changeFavoriteStatusHandle}
                 />
             }
         })
-    }, [navigation]);
+    }, [navigation, changeFavoriteStatusHandle]);
 
     return <ScrollView style={styles.rootContainer}>
         <Image source={{uri: selectedMeal.imageUrl}} style={styles.image}/>
